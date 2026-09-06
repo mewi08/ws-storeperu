b68030e1c54bb262614aa421f193d22d724cd23b
Melanie Elizabeth <melanietello2020@gmail.com>
2026-09-06 17:06:45 -0500

    refactor: cambiar mysql2 a versión promise

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require("express");
-const mysql = require("mysql2");
+const mysql = require('mysql2/promise');
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
