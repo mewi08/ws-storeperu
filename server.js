631151f263a6f6acd7b39c77ade9ea553328d5ca
Melanie Elizabeth <melanietello2020@gmail.com>
2026-09-06 14:25:43 -0500

    chore: agregar configuración de base de datos y servidor

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const PORT = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.getConnection((err) => {
+  if (err) throw err;
+  console.log("Conectado a la base de datos Store Perú");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en http://localhost:${PORT}`)
+})
